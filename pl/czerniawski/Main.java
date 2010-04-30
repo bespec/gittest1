0a7deab74ceb53ed335d170e3968f5f49903ea42
Andrzej Czerniawski <a.czerniawski@zeto.koszalin.pl>
2010-04-30 23:28:06 +0200

    Signed-off-by: Andrzej Czerniawski <a.czerniawski@zeto.koszalin.pl>

--- pl/czerniawski/Main.java
+++ pl/czerniawski/Main.java
@@ -4,10 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("pierwsza klasa!");
-		System.out.println("pierwsza klasa!");
-		System.out.println("pierwsza klasa!");
-		System.out.println("pierwsza klasa!");
-		System.out.println("pierwsza klasa!");
 	}
 
 }
