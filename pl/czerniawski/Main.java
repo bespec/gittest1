9456b35afc0d7f9ee42dd22a182b3d32d962c6bb
Andrzej Czerniawski <a.czerniawski@zeto.koszalin.pl>
2010-05-01 22:37:33 +0200

    Signed-off-by: Andrzej Czerniawski <a.czerniawski@zeto.koszalin.pl>

--- pl/czerniawski/Main.java
+++ pl/czerniawski/Main.java
@@ -1,7 +1,11 @@
 package pl.czerniawski;
 
+import javax.swing.JFrame;
+
 public class Main {
 
+	private JFrame okno;
+	
 	public static void main(String[] args) {
 		System.out.println("pierwsza klasa!");
 	}
